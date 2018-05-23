3440b6dce5d44529a5c36f5a25c8791fe7c20fef
Matthew Grossman <grossmanmatt@yahoo.com>
2018-05-22 22:13:14 -0400

    added module to reduce repetitive code

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -1,6 +1,5 @@
-class Api::TodoItemsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  before_filter :find_todo_list
+class Api::TodoItemsController < Api::ApiController
+    before_filter :find_todo_list
 
 
   def create
